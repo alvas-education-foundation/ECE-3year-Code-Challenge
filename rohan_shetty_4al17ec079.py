9c1d58f2496300c055a702396e749a6cbf69c974
ROHAN SHETTY <53072949+ROHAN-SHETTY79@users.noreply.github.com>
2020-05-27 17:35:58 +0530

    Add files via upload

---
+++ rohan_shetty_4al17ec079.py
@@ -0,0 +1,21 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> username = "Micheal"
+password = "e3$WT89x"
+c = 0
+while c < 3:
+    a = input("enter the username")
+    b = input("enter the password")
+    if a == username and b == password:
+        print("loged in successfully ")
+        break
+    else:
+        c += 1
+        if c ==3:
+            print("you are blocked")
+        else:
+            print("try again")
+
+
+
+
