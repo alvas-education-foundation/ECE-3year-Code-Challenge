b63f5d86c6043b655503a1afa67175af2f048bb6
BHOOMIKA <54442992+bhoomikahebbar@users.noreply.github.com>
2020-05-28 10:08:16 +0530

    congo

---
+++ bhoomika_4al17ec010.py
@@ -0,0 +1,17 @@
+def login (user_name):
+print('enter user name')
+def login (password):
+print('enter correct password');
+count=0
+while count<3:
+user_name=input('enter user name:')
+if user_name=='Micheal':
+password=input('enter password;')
+if password=='e3$WT89x':
+count=5
+elif password!=1:
+print("incorrect")
+count+=1
+else:
+print("you tried too many times,locked")
+count>=3
